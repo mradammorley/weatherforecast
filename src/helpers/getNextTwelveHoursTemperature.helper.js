aeae29d63e314850a2833ef53ce45d0618909c92
Adam Morley <adam@adammorley.co.uk>
2018-10-07 23:08:22 +0100

    getNextTwelveHoursTemperature() passes

--- src/helpers/getNextTwelveHoursTemperature.helper.js
+++ src/helpers/getNextTwelveHoursTemperature.helper.js
@@ -1,2 +1,16 @@
 export const getNextTwelveHoursTemperature = (forecast) => {
+    if (typeof forecast !== "object") {
+        return false;
+    }
+
+    let data = [];
+    for (let i = 0; i < 4; i++) {
+        let measurement = {};
+        let date = new Date(forecast.list[i].dt * 1000);
+        measurement["time"] = date.getHours();
+        measurement["temp"] = forecast.list[i].main.temp
+        data.push(measurement);
+    }
+
+    return data;
 };
