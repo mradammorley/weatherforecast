a6a527038fd2fb009b4d9038de8a7e27e64556fe
Adam Morley <adam@adammorley.co.uk>
2018-10-07 20:59:05 +0100

    Failing test for TemperatureClock component

---
+++ src/components/TemperatureClock.test.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {mount} from "enzyme";
+import {Provider} from "react-redux";
+import {store} from "../helpers/store";
+import TemperatureClock from "./TemperatureClock";
+
+describe("App component", () => {
+    let mountedComponent;
+
+    beforeEach(() => {
+        mountedComponent = mount(
+            <Provider store={store}>
+                <TemperatureClock />
+            </Provider>,
+        );
+    });
+
+    it('Should render successfully', () => {
+        expect(mountedComponent.exists()).toEqual(true);
+    });
+
+});
