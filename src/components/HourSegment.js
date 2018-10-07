7a8f9713d923375cb7fbc9af2558a54e448d5023
Adam Morley <adam@adammorley.co.uk>
2018-10-07 21:07:46 +0100

    Added HourSegment, test passes

---
+++ src/components/HourSegment.js
@@ -0,0 +1,16 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+class HourSegment extends React.Component {
+
+    render() {
+        return (
+            <div className='HourSegment' />
+        );
+    }
+}
+
+HourSegment.propTypes = {
+};
+
+export default HourSegment;
