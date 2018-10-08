377bfd7ee8e67ca35db9005827d9050b12a50979
Adam Morley <adam@adammorley.co.uk>
2018-10-08 02:36:31 +0100

    Create svg polygons for hour segments

--- src/components/HourSegment.js
+++ src/components/HourSegment.js
@@ -4,13 +4,34 @@ import PropTypes from "prop-types";
 class HourSegment extends React.Component {
 
     render() {
+        const rotation = "rotate(" + this.props.hour * 30 + "deg)";
+        const svgStyle = {
+            position: "absolute",
+            transformOrigin: "0% 100%",
+            transform: rotation
+        };
         return (
-            <div className='HourSegment' />
+            <svg
+                className="HourSegment"
+                data-name={"segment-" + this.props.hour}
+                width="50"
+                height="100"
+                viewBox="0 0 50 100"
+                style={svgStyle}
+            >
+                <polygon
+                    points="0 100 50 13.2 0 0 0 100"
+                    fill={"hsl(" + this.props.hue + ", 100%, 50%)"}
+                />
+            </svg>
         );
     }
 }
 
 HourSegment.propTypes = {
+    hue: PropTypes.number,
+    temp: PropTypes.number,
+    hour: PropTypes.number
 };
 
 export default HourSegment;
